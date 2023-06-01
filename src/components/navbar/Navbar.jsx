09a00cdfd23dad0b8852cf5abbc21113836fa81b
YugenFox <kalebsday@gmail.com>
2023-05-31 22:52:36 -0500

    Added assets folder, about to add Navbar @media qs

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,73 @@
-import './navbar.css'
+import { useState } from "react";
+import "./navbar.css";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+
+//BEM -> Block Element Modifier
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT3?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
-export default Navbar
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="gpt3__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="gpt3__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#ffff"
+            size={27}
+            onclick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#ffff"
+            size={27}
+            onclick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menue_contrainer-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Navbar;
