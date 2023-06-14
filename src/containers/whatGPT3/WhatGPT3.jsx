efae6ae1e8fe5dbaf4e9524a6d67edfc737df862
YugenFox <kalebsday@gmail.com>
2023-06-14 17:21:19 -0500

    WhatGPT3's Feature component done, starting Features container
    2:00:00

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,7 +5,7 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title="What is GPT-3" text="(Generative Pre-trained Transformer 3) is a state-of-the-art AI model developed by OpenAI. It is one of the most powerful language models to date, capable of generating human-like text, understanding natural language, and performing a wide range of tasks, including translation, question answering, and text completion."/>
       </div>
       <div className="gpt3__whatgpt3-heading">
         <h1 className="gradient__text">
@@ -14,9 +14,9 @@ const WhatGPT3 = () => {
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title="Chatbots" text="Streamline customer support, handling inquiries, and delivering personalized assistance, improving customer satisfaction and reducing workload for agents."/>
+        <Feature title="Knowledgebase" text="Efficiently organize and retrieve information, empowering users with easy access to vast amounts of data, promoting knowledge sharing and self-service capabilities."/>
+        <Feature title="Education" text="Systems offer personalized and adaptive learning experiences, tailoring content and assessments to individual needs, enhancing engagement, comprehension, and knowledge retention."/>
       </div>
     </div>
   );
